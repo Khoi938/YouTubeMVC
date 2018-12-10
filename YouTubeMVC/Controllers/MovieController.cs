c15e2cddb67f8bafcd67d9981317e13e1fb0d41e
Khoi Le <khoi.le@sagitec.com>
2018-12-10 10:50:45 -0500

    Testing

---
+++ YouTubeMVC/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YouTubeMVC.Models;
+
+namespace YouTubeMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random1
+        public ActionResult Random1()
+        {
+            Movie movie = new Movie() { Name = "Alladin", Id = 1 };
+            return View(movie);
+        }
+    }
+}
